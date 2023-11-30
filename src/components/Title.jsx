a612e6000d53166b32e64947e9beefeead5be4b5
MauricioPerezCaram <perezcarammauricio@gmail.com>
2023-11-30 20:53:36 -0300

    falta responsive

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,9 +2,13 @@ import React from "react";
 
 function Title() {
   return (
-    <div>
+    <div className="titulo-y-logo">
       <h1 className="tituloppal">No olvides tus tareas con To Do App!</h1>
-      <h2 className="titulosecundario">Agrega, edita, tacha y elimina tus tareas para poder realizarlas de manera ordenada.</h2>
+      <h2 className="titulosecundario">
+        Agrega, edita, tacha y elimina tus tareas para poder realizarlas de
+        manera ordenada.
+      </h2>
+      <img src="done.jpg" alt="done" />
     </div>
   );
 }
